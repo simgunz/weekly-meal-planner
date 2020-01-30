1b498d44cbe695d40d4633bfc73281f463d4118e
Simone Gaiarin <simgunz@gmail.com>
2020-01-30 12:29:00 +0100

    Add Recipe schema, add create recipe view

---
+++ models/recipes.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const RecipeSchema = new Schema({
+  name: String,
+  text: String
+});
+
+mongoose.model('Recipe', RecipeSchema);
+
--- routes/recipes.js
+++ routes/recipes.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
+const mongoose = require('mongoose');
+
+const Recipe = mongoose.model('Recipe');
 
 /* GET receips listing. */
 router.get('/', function(req, res, next) {
   res.render('recipes', {title: 'Recipes', recipes: ['Hamburger', 'Patate']});
+});
+
+router.get('/create', function(req, res, next) {
+  res.render('recipes-create', {title: 'Create Recipe'});
+});
+
+router.post('/create', function(req, res) {
+  let newRecipe = Recipe(req.body).save(function(err, data) {
+      if (err) throw err;
+      res.json(data);
+  });
 });
 
 module.exports = router;
