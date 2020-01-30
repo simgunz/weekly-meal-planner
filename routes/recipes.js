e1c39bccb9e06fe6a7e4dc352a46f2c8a11653f3
Simone Gaiarin <simgunz@gmail.com>
2020-01-30 10:32:31 +0100

    Add recipes page

--- routes/recipes.js
+++ routes/recipes.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET receips listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('recipes', {title: 'Recipes', recipes: ['Hamburger', 'Patate']});
 });
 
 module.exports = router;
