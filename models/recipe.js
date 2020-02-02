cbada19db6b7bc088311fa9083d2af88cae67117
Simone Gaiarin <simgunz@gmail.com>
2020-02-02 19:20:25 +0100

    Fix schema for servedWith

--- models/recipe.js
+++ models/recipe.js
@@ -7,7 +7,7 @@ const RecipeSchema = new Schema({
   servings: Number,
   course: String,
   category: String,
-  servedWith: { type: Schema.Types.ObjectId, ref: 'Recipe', required: false },
+  servedWith: [{ type: Schema.Types.ObjectId, ref: 'Recipe', required: false }],
   ingredients: [String],
   instructions: String,
 });
