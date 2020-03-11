587bf6d7414b2df78e2026bacf6c5c259a37fa30
michele <michele.gaiarin@gmail.com>
2020-03-11 17:22:34 +0100

    Setting up Passport.js

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,50 @@
+/*
+ * Required External Modules
+ */
 const express = require('express');
 
 const router = express.Router();
+
+// Authentication modules 
+const expressSession = require("express-session");
+const passport = require("passport");
+const Auth0Strategy = require("passport-auth0");
+
+require("dotenv").config();
+
+/**
+ * App Variables
+ */
+
+const app = express();
+const port = process.env.PORT || "9229";
+
+/**
+ * Session Configuration
+ */
+
+const session = {
+  secret: "LoxodontaElephasMammuthusPalaeoloxodonPrimelephas",
+  cookie: {},
+  resave: false,
+  saveUninitialized: false
+};
+
+if (app.get("env") === "production") {
+  // Serve secure cookies, requires HTTPS
+  session.cookie.secure = true;
+}
+
+/**
+ *  App Configuration
+ */
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(expressSession(session));
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
