84c21fb56b1bd047f23aec2a55fc42df3cc41ee3
michele <michele.gaiarin@gmail.com>
2020-03-12 00:13:55 +0100

    Fix style and missing var

--- routes/index.js
+++ routes/index.js
@@ -5,43 +5,69 @@ const express = require('express');
 
 const router = express.Router();
 
-// Authentication modules 
-const expressSession = require("express-session");
-const passport = require("passport");
-const Auth0Strategy = require("passport-auth0");
+// Authentication modules
+const expressSession = require('express-session');
+const passport = require('passport');
+const Auth0Strategy = require('passport-auth0');
 
-require("dotenv").config();
+require('dotenv').config();
 
 /**
  * App Variables
  */
 
 const app = express();
-const port = process.env.PORT || "9229";
+const port = process.env.PORT || '9229';
+const path = require('path');
 
 /**
  * Session Configuration
  */
 
 const session = {
-  secret: "LoxodontaElephasMammuthusPalaeoloxodonPrimelephas",
+  secret: 'LoxodontaElephasMammuthusPalaeoloxodonPrimelephas',
   cookie: {},
   resave: false,
-  saveUninitialized: false
+  saveUninitialized: false,
 };
 
-if (app.get("env") === "production") {
+if (app.get('env') === 'production') {
   // Serve secure cookies, requires HTTPS
   session.cookie.secure = true;
 }
 
 /**
+ * Passport Configuration
+ */
+
+const strategy = new Auth0Strategy(
+  {
+    domain: process.env.AUTH0_DOMAIN,
+    clientID: process.env.AUTH0_CLIENT_ID,
+    clientSecret: process.env.AUTH0_CLIENT_SECRET,
+    callbackURL:
+      process.env.AUTH0_CALLBACK_URL || 'http://localhost:3000/callback',
+  },
+  function(accessToken, refreshToken, extraParams, profile, done) {
+    /**
+     * Access tokens are used to authorize users to an API
+     * (resource server)
+     * accessToken is the token to call the Auth0 API
+     * or a secured third-party API
+     * extraParams.id_token has the JSON Web Token
+     * profile has all the information from the user
+     */
+    return done(null, profile);
+  }
+);
+
+/**
  *  App Configuration
  */
 
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "pug");
-app.use(express.static(path.join(__dirname, "public")));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(expressSession(session));
 
@@ -56,31 +82,6 @@ passport.serializeUser((user, done) => {
 passport.deserializeUser((user, done) => {
   done(null, user);
 });
-
-/**
- * Passport Configuration
- */
-
-const strategy = new Auth0Strategy(
-  {
-    domain: process.env.AUTH0_DOMAIN,
-    clientID: process.env.AUTH0_CLIENT_ID,
-    clientSecret: process.env.AUTH0_CLIENT_SECRET,
-    callbackURL:
-      process.env.AUTH0_CALLBACK_URL || "http://localhost:3000/callback"
-  },
-  function(accessToken, refreshToken, extraParams, profile, done) {
-    /**
-     * Access tokens are used to authorize users to an API
-     * (resource server)
-     * accessToken is the token to call the Auth0 API
-     * or a secured third-party API
-     * extraParams.id_token has the JSON Web Token
-     * profile has all the information from the user
-     */
-    return done(null, profile);
-  }
-);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
