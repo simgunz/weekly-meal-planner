fc2fc64b2323fa3232905a7f20875c5bc3e66759
michele <michele.gaiarin@gmail.com>
2020-03-11 17:34:36 +0100

    Add passport config

--- routes/index.js
+++ routes/index.js
@@ -45,6 +45,42 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use(expressSession(session));
 
+passport.use(strategy);
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
+
+/**
+ * Passport Configuration
+ */
+
+const strategy = new Auth0Strategy(
+  {
+    domain: process.env.AUTH0_DOMAIN,
+    clientID: process.env.AUTH0_CLIENT_ID,
+    clientSecret: process.env.AUTH0_CLIENT_SECRET,
+    callbackURL:
+      process.env.AUTH0_CALLBACK_URL || "http://localhost:3000/callback"
+  },
+  function(accessToken, refreshToken, extraParams, profile, done) {
+    /**
+     * Access tokens are used to authorize users to an API
+     * (resource server)
+     * accessToken is the token to call the Auth0 API
+     * or a secured third-party API
+     * extraParams.id_token has the JSON Web Token
+     * profile has all the information from the user
+     */
+    return done(null, profile);
+  }
+);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
