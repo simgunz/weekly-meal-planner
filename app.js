283891831bc9c89d064e3d66aa4cf053a58b4872
michele <michele.gaiarin@gmail.com>
2020-03-13 18:18:38 +0100

    Setted up Passport.js

--- app.js
+++ app.js
@@ -5,6 +5,14 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const mongoose = require('mongoose');
 
+// Login authentication
+const expressSession = require('express-session');
+const passport = require('passport');
+const Auth0Strategy = require('passport-auth0');
+
+// load the environmental variables stored in .env
+require('dotenv').config();
+
 // Connect to the database
 const dbAddress =
   'mongodb+srv://simone:lJmrNCrhiv6FpUe3@cluster0-qbm8s.mongodb.net/meal_planner?retryWrites=true&w=majority';
@@ -47,6 +55,65 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+});
+
+/**
+ * Passport Configuration
+ */
+
+const strategy = new Auth0Strategy(
+  {
+    domain: process.env.AUTH0_DOMAIN,
+    clientID: process.env.AUTH0_CLIENT_ID,
+    clientSecret: process.env.AUTH0_CLIENT_SECRET,
+    callbackURL:
+      process.env.AUTH0_CALLBACK_URL || 'http://localhost:3000/callback',
+  },
+  function(accessToken, refreshToken, extraParams, profile, done) {
+    /**
+     * Access tokens are used to authorize users to an API
+     * (resource server)
+     * accessToken is the token to call the Auth0 API
+     * or a secured third-party API
+     * extraParams.id_token has the JSON Web Token
+     * profile has all the information from the user
+     */
+    return done(null, profile);
+  }
+);
+
+/**
+ * Session Configuration
+ */
+
+const session = {
+  secret: 'LoxodontaElephasMammuthusPalaeoloxodonPrimelephas',
+  cookie: {},
+  resave: false,
+  saveUninitialized: false,
+};
+
+if (app.get('env') === 'production') {
+  // Serve secure cookies, requires HTTPS
+  session.cookie.secure = true;
+}
+
+/**
+ *  App Configuration for Auth0
+ */
+
+passport.use(strategy);
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(expressSession(session));
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+  done(null, user);
 });
 
 module.exports = app;
